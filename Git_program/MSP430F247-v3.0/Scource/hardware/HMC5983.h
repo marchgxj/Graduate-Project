31e016db8d93293fcb3792b63c686906422a7680
Xiaoxiami <chang-xiao-dong@hotmail.com>
2015-11-02 13:07:10 +0800

    rm msp430-v3.0

--- Git_program/MSP430F247-v3.0/Scource/hardware/HMC5983.h
+++
@@ -1,8 +0,0 @@
-#ifndef HMC5983_H
-#define HMC5983_H
-#include "common.h"
-
-extern void Multi_Read_HMC(uint16* XValue,uint16* YValue,uint16* ZValue);
-extern void Init_5983();
-extern void Single_Read_HMC(uint16* XValue,uint16* YValue,uint16* ZValue);
-#endif
